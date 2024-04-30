e77be70728d109ca3d76cae610d8ec8a1bf181cc
seeker8 <chuyo_gk87@yahoo.com.mx>
2024-04-29 22:22:33 -0400

    define query and add comments for Track and Author fields

--- server/src/schema.js
+++ server/src/schema.js
@@ -6,18 +6,30 @@ const typeDefs = gql`
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
+    "the track's title"
     title: String!
+    "The track's main author"
     autor: Author!
+    "The track's main illustration to display in track card or track page detail"
     thumbnail: String
+    "The track's approximate length to complete, in minutes"
     length: Int
+    "The number of modules this track contains"
     modulesCount: Int
   }
-  
+
   "Author of a complete Track or a Module"
   type Author {
     id: ID!
+    "Author's first and last name"
     name: String!
+    "Author's profile picture url"
     photo: String
+  }
+
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
   }
 `;
 
