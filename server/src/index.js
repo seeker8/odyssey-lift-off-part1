b96f92e47d5f77785a0f16e1232806666032330d
seeker8 <chuyo_gk87@yahoo.com.mx>
2024-04-30 08:46:18 -0400

    add startApolloServer function

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,7 @@
-//TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import typeDefs from "./schema";
+
+async function startApolloServer() {
+    const server = new ApolloServer({ typeDefs });
+}
