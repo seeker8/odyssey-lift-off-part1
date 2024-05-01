0a64b7e880b5d7d9ed9c5e4affde5082da94d0fe
seeker8 <chuyo_gk87@yahoo.com.mx>
2024-04-30 21:41:03 -0400

    update dependencies on server/ package.json

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,15 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { addMocksToSchema } from '@graphql-tools/mock';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 import { typeDefs } from "./schema.js";
 
 async function startApolloServer() {
-    const server = new ApolloServer({ typeDefs });
+    const server = new ApolloServer({ 
+        schema: addMocksToSchema({
+            schema: makeExecutableSchema({ typeDefs })
+        })
+     });
     const { url } = await startStandaloneServer(server);
     console.log(`
         🚀  Server is running!
