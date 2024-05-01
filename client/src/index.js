db52ea690527b4868cc9e01efced819d6f33c1aa
seeker8 <chuyo_gk87@yahoo.com.mx>
2024-04-30 23:26:47 -0400

    define an ApolloClient instance

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloClient, InMemoryCache, AolloProvider } from '@apollo/client';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+})
 const root = createRoot(document.getElementById('root'));
 
 root.render(
