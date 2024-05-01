13da236031bee4811156874f34e82f3c3e2245a9
seeker8 <chuyo_gk87@yahoo.com.mx>
2024-04-30 22:01:37 -0400

    add mocks

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,14 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { addMocksToSchema } from '@graphql-tools/mock';
 import { makeExecutableSchema } from '@graphql-tools/schema';
+import { mocks } from './mocks.js';
 import { typeDefs } from "./schema.js";
 
 async function startApolloServer() {
     const server = new ApolloServer({ 
         schema: addMocksToSchema({
-            schema: makeExecutableSchema({ typeDefs })
+            schema: makeExecutableSchema({ typeDefs }),
+            mocks
         })
      });
     const { url } = await startStandaloneServer(server);
---
+++ server/src/mocks.js
@@ -0,0 +1,15 @@
+export const mocks = {
+    Track: () => ({
+        id: () => "track_01",
+        title: () => "Astro Kitty, Space Explorer",
+        author: () => {
+            return {
+                name: "Grumpy Cat",
+                photo: "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg", 
+            };
+        },
+        thumbnail: () => "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+        length: () => 1210,
+        moduleCount: () => 6
+    })
+}
