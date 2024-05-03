95c3d23963cda6c8261c9c1e1752d0adf0d46eaa
seeker8 <chuyo_gk87@yahoo.com.mx>
2024-05-02 20:55:22 -0400

    setup datasources to retrieve data

---
+++ server/src/datasources/track-api.js
@@ -0,0 +1,13 @@
+import { RESTDataSource } from '@apollo/datasource-rest';
+
+export class TrackAPI extends RESTDataSource {
+    baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+
+    getTacksForHome() {
+        return this.get('tracks');
+    }
+
+    getAuthor(authorId) {
+        return this.get(`author/${encodeURIComponent(authorId)}`);
+    }
+}
