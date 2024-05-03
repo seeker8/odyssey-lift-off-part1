32ff3102b6e04e252e6cda5ef40e24b0d71d88dc
seeker8 <chuyo_gk87@yahoo.com.mx>
2024-05-02 22:21:33 -0400

    setup server to use rest api

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,36 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { addMocksToSchema } from '@graphql-tools/mock';
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { mocks } from './mocks.js';
+import { resolvers } from './resolvers.js';
+import { TrackAPI } from './datasources/track-api.js';
+// these imports are used to setup mocked response
+// import { addMocksToSchema } from '@graphql-tools/mock';
+// import { makeExecutableSchema } from '@graphql-tools/schema';
+// import { mocks } from './mocks.js';
 import { typeDefs } from "./schema.js";
 
 async function startApolloServer() {
-    const server = new ApolloServer({ 
-        schema: addMocksToSchema({
-            schema: makeExecutableSchema({ typeDefs }),
-            mocks
-        })
-     });
-    const { url } = await startStandaloneServer(server);
+    // this setup mocks
+    // const server = new ApolloServer({ 
+    //     schema: addMocksToSchema({
+    //         schema: makeExecutableSchema({ typeDefs }),
+    //         mocks
+    //     })
+    //  });
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers
+    });
+    const { url } = await startStandaloneServer(server, {
+        context: async () => {
+            const { cache } = server;
+            return {
+                dataSources: {
+                    trackAPI: new TrackAPI({ cache })
+                }
+            };
+        }
+    });
     console.log(`
         🚀  Server is running!
         📭  Query at ${url}
