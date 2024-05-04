6d44b94bcefe2c4f1cc51c92d787c7c29c766dde
seeker8 <chuyo_gk87@yahoo.com.mx>
2024-05-04 13:16:07 -0400

    add query to Track component

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -3,6 +3,30 @@ import { gql, useQuery } from '@apollo/client';
 import { Layout, QueryResult } from "../components";
 import { useParams } from "react-router-dom";
 
+const GET_TRACK = gql`
+  query GetTracks($trackId: ID!) {
+      track(id: $trackId) {
+          id
+          title
+          author {
+              id
+              name
+              photo
+          }
+          thumbnail
+          length
+          modulesCount
+          description
+          numberOfViews
+          modules {
+              id
+              title
+              length
+          }
+      }
+  }
+`;
+
 const Track = () => {
     const { trackId = '' } = useParams();
     return <Layout></Layout>;
