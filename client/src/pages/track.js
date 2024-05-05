a01e1672edc0f60069a854846ae89fef6bd2c10c
seeker8 <chuyo_gk87@yahoo.com.mx>
2024-05-05 12:42:55 -0400

    add TrackDetail component to render the track's details

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { gql, useQuery } from '@apollo/client';
-import { Layout, QueryResult } from "../components";
 import { useParams } from "react-router-dom";
+import Layout from "../components/layout";
+import QueryResult from "../components/query-result";
+import TrackDetail from "../components/track-detail";
 
 const GET_TRACK = gql`
   query GetTracks($trackId: ID!) {
@@ -29,7 +31,16 @@ const GET_TRACK = gql`
 
 const Track = () => {
     const { trackId = '' } = useParams();
-    return <Layout></Layout>;
+    const { loading, error, data } = useQuery(GET_TRACK, {
+      variables: {
+        trackId
+      }
+    });
+    return (<Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>);
 }
 
 export default Track;
