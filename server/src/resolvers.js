1cd4cf28a85bf25f6b46086922a06576c647b3ef
seeker8 <chuyo_gk87@yahoo.com.mx>
2024-05-05 19:08:39 -0400

    update increment mutation to handle errors

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -16,12 +16,21 @@ export const resolvers = {
     },
     Mutation: {
       incrementTrackViews:async (_, { id }, { dataSources }) => {
-        const track = await dataSources.trackAPI.incrementTrackViews(id);
-        return {
-          code: 200,
-          success: true,
-          message: `Successfully incremented number of views for track ${id}`,
-          track
+        try {
+          const track = await dataSources.trackAPI.incrementTrackViews(id);
+          return {
+            code: 200,
+            success: true,
+            message: `Successfully incremented number of views for track ${id}`,
+            track
+          }
+        }catch(err){
+          return {
+            code: err.extensions.response.status,
+            success: false,
+            message: err.extensions.response.body,
+            track: null
+          };
         }
       }
     },
